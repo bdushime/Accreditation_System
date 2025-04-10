0bd2525ee4db467fecd5730df6783fe53b2a400c
Dushime Beni <bdushime47@gmail.com>
2025-04-10 18:12:32 +0200

    added MailKit package in the project setup

--- obj/Release/net8.0/AccreditationSystem.AssemblyInfo.cs
+++ obj/Release/net8.0/AccreditationSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccreditationSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174009a001027ff1a8fd1130dcbac0d4316e6bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccreditationSystem")]
