a2d6ee720407426f0948cb20a341c5dc6df7d177
Dushime Beni <bdushime47@gmail.com>
2025-04-11 15:47:42 +0200

    resolving main comflicts

--- obj/Debug/net8.0/AccreditationSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/AccreditationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccreditationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd2525ee4db467fecd5730df6783fe53b2a400c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3845276239cb9f58af61648008bab957555bca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccreditationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccreditationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
